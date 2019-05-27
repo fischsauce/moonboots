d6abf1aa25be9b64259d0bc278fe28a8aa54be72
Fishsauce <36834391+fischsauce@users.noreply.github.com>
2019-05-27 16:05:13 +0800

    Update and rename eye7.js to eye8.js

--- eye8.js
+++ eye8.js
@@ -4,7 +4,7 @@ var textTL = new TimelineMax({repeat:-1, repeatDelay:0});
 
 var eye_inner = new Array ("#iris", "#pupil");
 
-eyeTL.to(eye_inner, 3, {x:+18, ease: Power2.easeInOut});
+eyeTL.to(eye_inner, 3, {x:+13, ease: Power2.easeInOut});
 //eyeTL.to(pupil, 0.5, {scale: 0.80, transformOrigin: "50% 50%"});
 //eyeTL.to(pupil, 0.5, {scale: 1, transformOrigin: "50% 50%"}, '+=1.5');
 eyeTL.to(eye_inner, 3, {x:-10, ease: Power2.easeInOut});
@@ -12,7 +12,7 @@ eyeTL.to(eye_inner, 3, {x:-10, ease: Power2.easeInOut});
 //eyeTL.to(pupil, 0.5, {scale: 1, transformOrigin: "50% 50%"}, '+=1.5');
 eyeTL.to(eye_inner, 3, {x:0, ease: Power2.easeInOut});
 
-glassTL.to(glass, 3, {x:+16, ease: Power2.easeInOut});
+glassTL.to(glass, 3, {x:+12, ease: Power2.easeInOut});
 glassTL.to(glass, 3, {x:-8, ease: Power2.easeInOut});
 glassTL.to(glass, 3, {x:0, ease: Power2.easeInOut});
 
