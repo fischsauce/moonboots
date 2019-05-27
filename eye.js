a0dce50b31a295edbd8e726f248580154a520d8d
Fishsauce <36834391+fischsauce@users.noreply.github.com>
2019-05-27 15:47:55 +0800

    Update eye.js

--- eye.js
+++ eye.js
@@ -5,11 +5,11 @@ var textTL = new TimelineMax({repeat:-1, repeatDelay:0});
 var eye_inner = new Array ("#iris", "#pupil");
 
 eyeTL.to(eye_inner, 3, {x:+10, ease: Power2.easeInOut});
-eyeTL.to(pupil, 0.5, {scale: 0.80, transformOrigin: 'pupil'});
-eyeTL.to(pupil, 0.5, {scale: 1, transformOrigin: 'pupil'}, '+=1.5');
+eyeTL.to(pupil, 0.5, {scale: 0.80, transformOrigin:"50% 50%"});
+eyeTL.to(pupil, 0.5, {scale: 1, transformOrigin:"50% 50%"}, '+=1.5');
 eyeTL.to(eye_inner, 3, {x:-10, ease: Power2.easeInOut});
-eyeTL.to(pupil, 0.5, {scale: 0.75, transformOrigin: 'pupil'});
-eyeTL.to(pupil, 0.5, {scale: 1, transformOrigin: '[pupil'}, '+=1.5');
+eyeTL.to(pupil, 0.5, {scale: 0.75, transformOrigin:"50% 50%"});
+eyeTL.to(pupil, 0.5, {scale: 1, transformOrigin:"50% 50%"}, '+=1.5');
 eyeTL.to(eye_inner, 3, {x:0, ease: Power2.easeInOut});
 
 glassTL.to(glass, 3, {x:+16, ease: Power2.easeInOut});
